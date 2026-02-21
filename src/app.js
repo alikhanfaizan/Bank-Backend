4d4a64d096b41e9d6ad284c351c09f68c55d266b
Faizan Ali Khan <alikhanfaizan642@gmail.com>
2026-02-22 00:36:55 +0530

    Email Service and Account model Added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
 const app = express();
 
 app.use(express.json())
@@ -8,6 +9,7 @@ app.use( cookieParser() )
 
 
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 
 module.exports = app;
